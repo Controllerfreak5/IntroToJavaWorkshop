c11e93b62518f6d186a8e2220e42315a30441ea9
PotatoBob <league@WTS11.attlocal.net>
2016-04-12 14:58:40 -0700

    moving robots

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,34 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot metal = new Robot();
 
-	
-		
-		
+metal.setSpeed(10);
+
+
+metal.sparkle();
+
+metal.penDown();
+
+metal.setRandomPenColor();
+
+for (int i = 0; i < 7; i++) {
+	metal.move(90);
+	metal.turn(45);
+}
+
+metal.move(90);
+
+metal.penUp();
+
+metal.turn(-45);
+metal.move(90);
+metal.turn(90);
+metal.move(45);
+
+
+metal.penUp();
+
+
 	}
 }
